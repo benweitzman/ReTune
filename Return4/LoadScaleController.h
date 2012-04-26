db7231fd9468be5ba081ff04165e9a26d0571ae5
benweitzman <benjamin.weitzman@tufts.edu>
2012-04-25 22:12:47 -0400

    added in hotkeys and temp slots, moved Retune4.3 to master and master to storyboard

---
+++ Return4/LoadScaleController.h
@@ -0,0 +1,13 @@
+//
+//  LoadScaleController.h
+//  Retune4.3
+//
+//  Created by Ben Weitzman on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadScaleController : UITableViewController
+
+@end
