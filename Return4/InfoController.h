aead407f18334ae6d95a86f586ed008a632d4088
benweitzman <benjamin.weitzman@tufts.edu>
2012-06-26 10:56:31 -0400

    misc updates. nothing big started working on info page

---
+++ Return4/InfoController.h
@@ -0,0 +1,13 @@
+//
+//  InfoController.h
+//  Retune4.3
+//
+//  Created by Ben Weitzman on 6/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoController : UIViewController
+
+@end
